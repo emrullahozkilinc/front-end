3343d61d286e9ed5c0208d4ae923e9a9ce614fca
Emrullah <emr.ozk.71@gmail.com>
2022-04-24 05:42:43 +0300

    Education material

--- educationappfront/src/App.js
+++ educationappfront/src/App.js
@@ -6,35 +6,6 @@ import Users from "./components/educational/Users";
 class App extends React.Component{
     constructor(props) {
         super(props);
-
-        this.state = {
-            users : [
-                {
-                    id : 1,
-                    name : "Mustafa Ali",
-                    salary : "2600",
-                    department : "Dağıtım"
-                },
-                {
-                    id : 2,
-                    name : "Muhammet Girgin",
-                    salary : "9000",
-                    department : "Perakende"
-                },
-                {
-                    id : 3,
-                    name : "Emre Polat",
-                    salary : "5000",
-                    department : "Satış"
-                },
-                {
-                    id : 4,
-                    name : "Lale Yormaz",
-                    salary : "6500",
-                    department : "İnsan Kaynakları"
-                }
-            ]
-        }
     }
     render() {
         return (
