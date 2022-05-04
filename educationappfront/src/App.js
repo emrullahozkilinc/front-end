e42763e188acebb6e8f66d1f4e5a87f04459fa8e
Emrullah <emr.ozk.71@gmail.com>
2022-05-04 15:49:11 +0300

    Some educational material added.

--- educationappfront/src/App.js
+++ educationappfront/src/App.js
@@ -1,21 +1,42 @@
 import React from "react";
 import './App.css';
 import './components/User'
-import Users from "./components/educational/Users";
+import Table from "./components/educational/Table";
+import Button from "./components/educational/Button";
 
-class App extends React.Component{
-    constructor(props) {
-        super(props);
-    }
-    render() {
-        return (
-            <div className="container">
-                <Users users={this.state.users} key={this.state.users.id}/>
-            </div>
-        );
-    }
+function App(){
 
+    const users = [
+        {
+            id: 1,
+            name: 'John',
+            age: 30
+        },
+        {
+            id: 2,
+            name: 'Sara',
+            age: 25
+        },
+        {
+            id: 3,
+            name: 'Mike',
+            age: 27
+        },
+        {
+            id: 4,
+            name: 'Adam',
+            age: 29
+        }
+    ];
 
+    return (
+        <div>
+            <Button><Table user={users}/></Button>
+            <Button><Table user={users}/></Button>
+            <Button><Table user={users}/></Button>
+        </div>
+    );
 }
+
 
 export default App;
